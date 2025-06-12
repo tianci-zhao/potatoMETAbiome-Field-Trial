d542d0b5dc0a510ae514a3c0b02354d0cf8a881e
Tianci Zhao <91272660+tianci-zhao@users.noreply.github.com>
2025-06-12 12:58:05 +0200

    Update Figure 1 and S2 and S3.R

--- Figure 1 and S2 and S3.R
+++ Figure 1 and S2 and S3.R
@@ -166,46 +166,6 @@ summary(re.av)
 re.test <- duncan.test(re.av, trt = 'Variety')
 re.test
 
-#Figrue 1e
-library(ggpubr)
-library(tidyverse)
-library(ggpmisc)
-
-mytheme <- 
-  theme_bw() +
-  theme(text = element_text(size=14),
-        plot.title = element_text(size=14, face="bold"),
-        strip.background = element_blank(),
-        strip.text=element_text(face="bold", size = 14),
-        axis.text.x = element_text(angle = 0, vjust = 0.5, hjust=1),
-        legend.position = "none",
-        panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank())
-#week7
-df_all <- read.csv("MIT_scores.csv", header = 1, sep = ",")
-df_all$Treatment <- factor(df_all$Treatment, levels=c("Control", "Consortium B", "Consortium BP", "Fertilizers", "Pesticides", "Fertilizers-Pesticides"))
-
-#all trements
-(p <- ggplot(df_all, aes(x = MIT_ric_sha_ratio, y = Belowground_biomass )) +
-    #geom_text(aes(label = WHC), vjust = 1, hjust = -0.1, size = 2, alpha = 0.6) +
-    geom_smooth(method = "lm", color= "#08519c",alpha=0.1)+
-    geom_ribbon(stat = "smooth", method = "lm", alpha=0.1, color="#08519c", linetype = 2)+
-    #spearman/pearson method
-    stat_cor(method = "pearson",label.y = 8,size = 5,label.x=-0.5,
-             aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")))+
-    stat_regline_equation( label.y = 7, label.x=-0.5,size = 5)+ 
-    #geom_point(aes(fill = Treatment), size = 3, shape = 19, color="#3182bd", alpha = 0.8) +
-    geom_point(aes(color = Treatment), size = 5, shape = 19, alpha = 0.8) +
-    scale_color_manual(values = c("Control" = "#bfd2ec", "Consortium B" = "#a6c39d", "Consortium BP" = "#2a9c90","Fertilizers" = "#eab68e","Pesticides" = "#e2db47","Fertilizers-Pesticides" = "#ea8e94" )) +
-    #facet_wrap(facets= "Time_point", nrow=1,ncol=3)+
-    labs(x = "MIT (z score)", y = "Belowground biomass (g)", title = "") +
-    mytheme)
-
-
-ggsave("Cor_all_rich_sha_ratio_below_3.pdf", width = 15, height = 6, p)
-
-
-
 
 #Supplementary Figure 2####
 ##treatments in same variety####
